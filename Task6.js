7c44ca9b63eeec642764810cf204b79ee15159af
KhalezAlex <106075342+KhalezAlex@users.noreply.github.com>
2022-08-11 18:16:03 +0300

    Add files via upload

--- Task6.js
+++ Task6.js
@@ -24,27 +24,46 @@
     document.getElementById("route").appendChild(option);
   }
 
+  var routeOrder;
+  var dateOrder;
+  var modal = document.getElementById("message");
+  var btn = document.getElementById("orderButton");
+  var btnOrder = document.getElementById("close");
+  var price = 0;
+
   function getSeats(number) {
     let seats = new Array();
     for (let i = 0; i < 4; i++) {
       let cell = document.createElement("td");
       cell.setAttribute("class", "cell");
-
       let div = document.createElement("div");
       let cb = document.createElement("input");
       cb.setAttribute("type", "checkbox");
       cb.setAttribute("id", "seat" + (i + number));
       cb.setAttribute("name", "checkbox");
       if (Math.random() > 0.75) cb.setAttribute("disabled", "true");
-      div.appendChild(cb);
+      else cb.setAttribute("onclick", "setPrice(seat" + (i + number) + ")");
       let label = document.createElement("label");
       label.setAttribute("for", "checkbox");
       label.innerHTML = i + number;
+      div.appendChild(cb);
       div.appendChild(label);
       cell.appendChild(div);
       seats.push(cell);
     }
     return seats;
+  }
+
+  function setPrice(cbId) {
+    let index;
+    for (let i = 0; i < 4; i++) if (route[i][0] == routeOrder) index = i;
+    let str = document.getElementById("orderButton").innerHTML;
+    let str1 = str.substring(14);
+    if (document.getElementById(cbId.id).checked)
+      str1 = parseInt(str1) + route[index][1];
+    else str1 = parseInt(str1) - route[index][1];
+    str = str.substring(0, 14) + str1;
+    document.getElementById("orderButton").innerHTML = str;
   }
 
   function getCoupe(number) {
@@ -63,6 +82,7 @@
   function getCarriage() {
     let carriage = document.createElement("table");
     carriage.setAttribute("class", "coupe");
+    carriage.setAttribute("id", "carriage");
     let row = document.createElement("tr");
     for (let i = 1; i < 28; i += 4) {
       let cell = document.createElement("td");
@@ -70,27 +90,46 @@
       row.appendChild(cell);
     }
     carriage.appendChild(row);
+    carriage.style.marginTop = "10px";
     return carriage;
   }
-  var routeOrder;
-  var dateOrder;
 
-  function showCarriage() {
+  function drawCarriage() {
     routeOrder = document.getElementById("route").value;
     dateOrder = document.getElementById("date").value;
-    document.body.appendChild(getCarriage());
+    if (document.getElementById("seats") != null)
+      document.getElementById("seats").remove();
+    let divSeats = document.createElement("div");
+    divSeats.setAttribute("id", "seats");
+    divSeats.appendChild(getCarriage());
+    divSeats.appendChild(drawOrderBlock());
+    document.getElementById("container").appendChild(divSeats);
+  }
+
+  function drawOrderBlock() {
+    let div = document.createElement("div");
+    div.style.width = "100%";
+    div.style.display = "flex";
+    div.style.justifyContent = "right";
+    div.style.marginTop = "10px";
+
     let button = document.createElement("input");
     button.setAttribute("type", "button");
     button.setAttribute("value", "Сделать заказ");
     button.setAttribute("onclick", "order()");
     button.setAttribute("name", "orderButton");
     button.setAttribute("id", "orderButton");
-    document.body.appendChild(button);
-  }
 
-  var modal = document.getElementById("message");
-  var btn = document.getElementById("orderButton");
-  var btnColseOrder = document.getElementById("close");
+    let label = document.createElement("label");
+    label.setAttribute("for", "orderButton");
+    label.innerHTML = "Цена билетов: " + price;
+    label.style.marginRight = "10px";
+    label.setAttribute("id", "orderButton");
+
+    div.appendChild(label);
+    div.appendChild(button);
+    return div;
+  }
 
   function order() {
     let seatsBooked = 0;
@@ -102,50 +141,75 @@
         seats.push(seatsChecked[i]);
       }
     }
-    let totalPrice = seatsBooked * routeOrder[1];
-    for (let i = 0; i < route.length; i++) {
-      if (route[i][0] == routeOrder) {
-        totalPrice = seatsBooked * route[i][1];
-      }
-    }
+    drawTableTicketsBooked(seats);
+    drawModalCloseButton();
+    modal.style.display = "block";
+  }
+
+  function drawTableTicketsBooked(seats) {
     let table = document.createElement("table");
     table.setAttribute("class", "coupe");
+    table.setAttribute("id", "ticketsBooked");
     for (let i = 0; i < seats.length + 1; i++) {
       if (i == 0) {
-        let row = document.createElement("tr");
-        let cell1 = document.createElement("td");
-        cell1.setAttribute("class", "cell");
-        cell1.innerHTML = "Направление";
-        let cell2 = document.createElement("td");
-        cell2.innerHTML = "Дата";
-        cell2.setAttribute("class", "cell");
-
-        let cell3 = document.createElement("td");
-        cell3.innerHTML = "Места";
-        cell3.setAttribute("class", "cell");
-        row.appendChild(cell1);
-        row.appendChild(cell2);
-        row.appendChild(cell3);
-        table.appendChild(row);
+        drawTableTicketsBookedHeader(table);
       } else {
-        let row = document.createElement("tr");
-        let cell1 = document.createElement("td");
-        cell1.innerHTML = routeOrder;
-        cell1.setAttribute("class", "cell");
-        let cell2 = document.createElement("td");
-        cell2.innerHTML = dateOrder;
-        cell2.setAttribute("class", "cell");
-
-        let cell3 = document.createElement("td");
-        cell3.innerHTML = seats[i - 1].id.substring(4);
-        cell3.setAttribute("class", "cell");
-        row.appendChild(cell1);
-        row.appendChild(cell2);
-        row.appendChild(cell3);
-        table.appendChild(row);
+        drawTableTicketsBookedData(table, seats, i);
       }
     }
-    document.getElementById("message").appendChild(table);
-    modal.style.display = "block";
+    document.getElementById("modal-content").appendChild(table);
+  }
+
+  function drawTableTicketsBookedHeader(table) {
+    let row = document.createElement("tr");
+    let cell1 = document.createElement("td");
+    cell1.setAttribute("class", "cell");
+    cell1.innerHTML = "Направление";
+
+    let cell2 = document.createElement("td");
+    cell2.innerHTML = "Дата";
+    cell2.setAttribute("class", "cell");
+
+    let cell3 = document.createElement("td");
+    cell3.innerHTML = "Места";
+    cell3.setAttribute("class", "cell");
+
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    table.appendChild(row);
+  }
+
+  function drawTableTicketsBookedData(table, seats, index) {
+    let row = document.createElement("tr");
+    let cell1 = document.createElement("td");
+    cell1.innerHTML = routeOrder;
+    cell1.setAttribute("class", "cell");
+    let cell2 = document.createElement("td");
+    cell2.innerHTML = dateOrder;
+    cell2.setAttribute("class", "cell");
+    let cell3 = document.createElement("td");
+    cell3.innerHTML = seats[index - 1].id.substring(4);
+    cell3.setAttribute("class", "cell");
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    table.appendChild(row);
+  }
+
+  function drawModalCloseButton() {
+    let button = document.createElement("input");
+    button.setAttribute("type", "button");
+    button.setAttribute("value", "ОК");
+    button.setAttribute("onclick", "closeModal()");
+    button.setAttribute("id", "close");
+    document.getElementById("modal-content").appendChild(button);
+  }
+
+  function closeModal() {
+    document.getElementById("close").remove();
+    document.getElementById("ticketsBooked").remove();
+    document.getElementById("message").style.display = "none";
+    document.getElementById("seats").remove();
   }
 }
